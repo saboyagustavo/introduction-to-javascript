6f79a7ecb5986914ba6eb51ed3e8352acbbd1b75
Gustavo Saboya <saboya.gustavo@gmail.com>
2020-07-04 23:58:43 -0300

    Three essential ways of declare variables in JS

---
+++ variables_and_data_types.js
@@ -0,0 +1,6 @@
+//(1/10) as JS is a dynamic language, I don't need to especify which type of data the variable contain;
+//three essential ways of declare variables
+var year = 2020; 
+const birthYear = 1992; //interger
+let age = year - birthYear; 
+console.log(age)
